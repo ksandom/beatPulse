3cead9e23f9e9aa0d7160698202c297520454c3e
Kevin Sandom <kjsandom@gmail.com>
2017-08-07 15:53:12 +0200

    Switched to a tab view to clean up the interface in preparation for adding more detail.

--- app/src/main/java/com/funnyhacks/pulse/MainActivity.java
+++ app/src/main/java/com/funnyhacks/pulse/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +18,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
+        While it won't take long to start everything up, even on an old phone, we may as well set
+        the welcome message first so the user can see that while stuff gets loaded.
+        */
+        // Set welcome message.
+        String welcomeMessage = "Hi!";
+        welcomeMessage += "\n\nThis is a really basic heart beat monitor to give you results really fast." +
+                "\nThere are two are two distinct pulses that come close together. If you feel" +
+                " both just press the button once. (Otherwise the estimate will flail around)" +
+                " like a shirt caught in a car door." +
+                "\n\nIMPORTANT" +
+                "\nNothing in this app should be taken as medical advice. If you" +
+                " need medical advice or have any medical questions, please see your doctor.";
 
+        final TextView welcome = (TextView) findViewById(R.id.welcomeMessage);
+        welcome.setText(welcomeMessage);
+
+
+        // Set up the button.
         final Button button = (Button) findViewById(R.id.beat);
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +54,26 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+
+        // Set up tabs.
+        TabHost host = (TabHost)findViewById(R.id.tabHost);
+        host.setup();
+
+        TabHost.TabSpec spec;
+
+        //Tab 1
+        spec = host.newTabSpec("Welcome");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Welcome");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("Do it!");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Do it!");
+        host.addTab(spec);
 
     }
 
